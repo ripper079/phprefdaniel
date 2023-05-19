09a0b000dc5da0875fabbaf9faeb38ed8e8c7f87
daniel <daniel@oikarainen.se>
2023-05-19 15:32:37 +0200

    Add more echo examples

--- featuresinphp/01_echoprint.php
+++ featuresinphp/01_echoprint.php
@@ -1,10 +1,26 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
+
+//Declare a country variable
+$countries = "Sweden, Finland, Poland, Thailand";
+
 //Lets start with the basics
-echo 'Hello', ' ', ' PHP', ' lets have fun\n';
+echo 'Hello', ' ', ' PHP', ' lets have fun<br>';
+echo "Hello", " ", " PHP", " lets have more fun<br><br>";
+
+//Countries that rock
+echo 'Countries that rock $countries<br>';
+echo "Countries that rock $countries<br><br>";
+
+
 //Single value
-print 'One can print one value';
+$returnValueFromPrint =  print 'One can print one value<br>';
+print 'The return value is $returnValueFromPrint<br>';
+print 'The return value is ' . $returnValueFromPrint . '<br>';
+print "The return value is $returnValueFromPrint<br>";
+
+
 //Arrays
 print_r([11,15,39,44]);
 //More detailed info
@@ -12,4 +28,3 @@ var_dump('Hello');
 var_dump(true);
 //String representation
 var_export('Hello');
-?>
---
+++ playgorund.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Training</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>This is a playground for php training</h1>
+    <?php
+        $country = "Sweden";
+    echo 'The viking country=$country<br>';
+    echo "The viking country=$country<br>";
+    ?>
+</body>
+</html>
