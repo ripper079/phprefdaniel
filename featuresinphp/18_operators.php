1f974529d634e12e19d271f9d8e54826412d8959
daniel <daniel@oikarainen.se>
2023-05-21 20:59:37 +0200

    operators added

---
+++ featuresinphp/18_operators.php
@@ -0,0 +1,37 @@
+<?php
+
+//String operators
+// $a = "Hello";
+// $b = "World";
+// $c = $a . " " . $b;
+// echo $c;
+
+//Arithmetic operator
+// echo 1 + 2 . "<br>";            //Addition
+// echo 10 % 3 . "<br>";                    //Modulo
+// echo 5 **3 . "<br>";                    //To power of
+
+//Asignment ooperator
+// $aa = 2;
+// $aa *= 4;       //$aa = $aa*4
+// echo $aa;
+
+//Comparison operators
+$a = 4;
+$b = "4";
+
+if ($a == $b){
+
+}
+
+//Checks also data type when 3 characters
+if ($a === $b){
+    
+}
+//Comparison Operators
+//!=, <>, !== , < , >, <=, >=, <=>
+
+//Logical operators
+//https://www.php.net/manual/en/language.operators.logical.php
+//Incrementing/Decrementing Operators 
+https://www.php.net/manual/en/language.operators.increment.php
